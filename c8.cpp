c9ef08f78fca5e491d6b4074ce5af711c95b2233
IrfanKMI17 <113580587+IrfanKMI17@users.noreply.github.com>
2022-09-22 12:19:20 +0530

    Update c8.cpp

--- c8.cpp
+++ c8.cpp
@@ -1,17 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int c=10;
-	int d=20;
-	printf("%d,%d,%d,%d,%d,%d,%d,%d\n",c++,c++,++c,c++,--c,c--,--c,c++);
-	printf("%d",c);
+int i=1;
+int j=10;
+void fun()
+{
+	std:cout<<"Hola";
 }
-// 11, 10, 12, 8,12 , 10,12 , 10
-//12
-new line added
-second line added
+}
 
-thirld line new repo
 
-fourth line added////
 
