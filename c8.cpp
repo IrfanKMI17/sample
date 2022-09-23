0dee6bf85490d03ba917111d77d01f28cf9e088d
IrfanKMI17 <113580587+IrfanKMI17@users.noreply.github.com>
2022-09-23 08:50:40 +0530

    Update c8.cpp

--- c8.cpp
+++ c8.cpp
@@ -12,7 +12,39 @@ void fun()
 {
 	std:cout<<"Hola";
 }
+	int featureinfy2
+	{
+	}
+
+	int featureinfy
+	{
+	}
+	int featureinfy1
+	{
+	}
+	int newone()
+	{
+	}
+	
+	
+
+	int feature5{
+		return 5;
+	}
+	int feature6
+	{
+		return 7;
+	}
 }
+
+int featurn6{
+	return 7;
+}
+//hello from second account
+//hello from another time
+//third
+//this is the  final copy thank you
+
 
 
 
