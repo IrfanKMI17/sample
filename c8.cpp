5825afb14d248e794ac1ad44481714f1e6a0a059
IrfanKMI17 <113580587+IrfanKMI17@users.noreply.github.com>
2022-09-23 09:27:27 +0530

    Update c8.cpp

--- c8.cpp
+++ c8.cpp
@@ -1,5 +1,35 @@
+void fun()
+{
+	std:cout<<"Hola";
+}
+	int featureinfy2
+	{
+	}
 
-void funs()
+	int featureinfy
+	{
+	}
+	int featureinfy1
+	{
+	}
+	int newone()
+	{
+	}
+	
+	
+
+	int feature5{
+		return 5;
+	}
+	int feature6
+	{
+		return 7;
+	}
+}
+
+int featurn6{
+	return 7;
+}
 
 
 
